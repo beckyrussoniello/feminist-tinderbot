029c2c9a1269bedd63d11605da3d9ff89d28d397
Becky Russoniello <rebecca.russoniello@gmail.com>
2016-10-16 14:33:08 -0500

    height memes

---
+++ profile_memes.rb
@@ -0,0 +1,6 @@
+# HEIGHT
+IT_MATTERS = /(since|if) (I know |apparently )?(that|it) matters/i
+NOT_THAT_IT_MATTERS = /not that it matters/i
+I_DONT_CARE = /i (don\'t care | couldn\'t care less )how tall you are/i
+THOSE_ARE_TWO_SEPARATE_MEASUREMENTS = /those are two (separate|different) measurements/i
+YOU_CAN_WEAR_YOUR_HEELS = /(wear|your) (highest |tallest )?heels/
